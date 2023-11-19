398f37a2cd040862285662f4c8488a08052d5906
Tod Beardsley <todb@packetfu.com>
2023-11-19 16:49:43 -0600

    Create a helper script for updates
    Automate this some day
    
    Signed-off-by: Tod Beardsley <todb@packetfu.com>

---
+++ fetch-kev.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+json_file="cisa.json"
+log_file="kev.log"
+curl -o "$json_file" https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json
+
+if [ -n "$(git status --porcelain "$json_file")" ]; then
+    git add "$json_file"
+    git commit -m "Update $json_file"
+    git push origin
+    echo "$(date): $json_file has been changed and committed." | tee -a "$log_file"
+else
+    echo "$(date): No changes in $json_file." | tee -a "$log_file"
+fi
