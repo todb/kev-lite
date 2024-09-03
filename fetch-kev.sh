6fa365381f2d168fbc4056ecc207e2134cac5edd
Tod Beardsley <todb@packetfu.com>
2024-09-03 13:34:20 +0000

    Clean up error checking

--- fetch-kev.sh
+++ fetch-kev.sh
@@ -7,12 +7,14 @@ json_file="cisa.json"
 log_file="kev.log"
 curl -o "$json_file" https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json
 
-# Check for JSON lint errors
+# Check for JSON lint errors introduced in downloading from cisa.gov
 if ! jq empty "$json_file" >/dev/null 2>&1; then
     echo "$(date): JSON lint error in $json_file. Commit aborted." | tee -a "$log_file"
+    git checkout -- "$json_file"
     exit 1
 fi
 
+# If we're here we're all good for processing."
 if [ -n "$(git status --porcelain "$json_file")" ]; then
     git add "$json_file"
     git commit -m "Update $json_file"
